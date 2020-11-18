0f74fccd391c29718dee3ecf389bafb9229cb8d3
Michael Kabali <kabalimichael03@gmail.com>
2020-11-18 07:19:52 +0300

    get user location

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geocoder/geocoder.dart';
 import 'package:location/location.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -6,14 +7,29 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget { 
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MyHomePage(),
+      home: MyHomePage(), //GetArea(), //MapView(), //HomePage(),//
     );
+  }
+}
+
+class GetArea extends StatelessWidget {
+  void getArea() async {
+    // From a query
+    final query = "Arua";
+    var addresses = await Geocoder.local.findAddressesFromQuery(query);
+    var first = addresses.first;
+    print("Got new area ${first.featureName} : ${first.coordinates}");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    getArea();
+    return Container();
   }
 }
 
@@ -25,30 +41,60 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  LatLng _initialCameraPositin = LatLng(20.5937, 78.9629);
   GoogleMapController _controller;
   Location _location = Location();
+  String apiKey = 'AIzaSyDeNVsxwno3K_sUIT5J8XbMPgUVcLUEkUA';
+  LatLng _initialCameraPositin;
+  //  =
+  //     LatLng(_latitude,_longitude );
+
+  @override
+  void initState() {
+    super.initState();
+    getAreaCordinates();
+  }
 
   void _onMapCreated(GoogleMapController _cntlr) {
     _controller = _cntlr;
     _location.onLocationChanged.listen((l) {
-      _controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(target: LatLng(l.latitude, l.longitude), zoom: 15)));
+      _controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
+          target: _initialCameraPositin, // LatLng(l.latitude, l.longitude),
+          zoom: 15)));
+    });
+  }
+
+  void getAreaCordinates() async {
+    // From a query
+    final query = "Makerere";
+    var addresses = await Geocoder.local.findAddressesFromQuery(query);
+    var first = addresses.first;
+    print(addresses.length);
+    print("Got new area ${first.featureName} : ${first.coordinates}");
+    setState(() {
+      // _coordinates = first.coordinates;
+      _initialCameraPositin =
+          LatLng(first.coordinates.latitude, first.coordinates.longitude);
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    getAreaCordinates();
     return Scaffold(
       body: Container(
-        height: MediaQuery.of(context).size.height,
+        height: MediaQuery.of(context).size.height * 0.7,
         width: MediaQuery.of(context).size.width,
-        child: Stack(  
+        child: Stack(
           children: [
-            GoogleMap(  
-              initialCameraPosition: CameraPosition(target: _initialCameraPositin),
-              mapType: MapType.normal, 
-              onMapCreated: _onMapCreated,
-              myLocationEnabled: true,
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: GoogleMap(
+                initialCameraPosition:
+                    CameraPosition(target: _initialCameraPositin),
+                mapType: MapType.normal,
+                onMapCreated: _onMapCreated,
+                myLocationEnabled: true,
+              ),
             )
           ],
         ),
